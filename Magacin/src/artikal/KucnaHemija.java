7f4dec03beee45340a1fb39d5d883fc5802e188d
AcaVicic <acavicic@gmail.com>
2022-03-10 17:11:27 +0100

    Dodate metode equals, hashCode i toString.

--- Magacin/src/artikal/KucnaHemija.java
+++ Magacin/src/artikal/KucnaHemija.java
@@ -1,6 +1,7 @@
 package artikal;
 
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class KucnaHemija extends Artikal {
 	
@@ -13,5 +14,31 @@ public class KucnaHemija extends Artikal {
 	public void setRokTrajanja(GregorianCalendar rokTrajanja) {
 		this.rokTrajanja = rokTrajanja;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(rokTrajanja);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		return Objects.equals(rokTrajanja, other.rokTrajanja);
+	}
+
+	@Override
+	public String toString() {
+		return "Artikal [naziv=" + getNaziv() + ", sifra=" + getSifra() + ", opis=" + getOpis() + ", kolicina=" + getKolicina() + "rokTrajanja=" + rokTrajanja + "]";
+	}
+	
 	
 }
