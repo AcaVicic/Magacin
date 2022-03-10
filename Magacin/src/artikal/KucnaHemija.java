16f7194628973c7cdb7cda4b7c9136e9830562ed
AcaVicic <acavicic@gmail.com>
2022-03-10 17:03:21 +0100

    Napravljene klase Artikal, Kancelarijski materijal, Knjiga i Kucna hemija i get i set metode za sve njihove atribute.

---
+++ Magacin/src/artikal/KucnaHemija.java
@@ -0,0 +1,17 @@
+package artikal;
+
+import java.util.GregorianCalendar;
+
+public class KucnaHemija extends Artikal {
+	
+	private GregorianCalendar rokTrajanja;
+
+	public GregorianCalendar getRokTrajanja() {
+		return rokTrajanja;
+	}
+
+	public void setRokTrajanja(GregorianCalendar rokTrajanja) {
+		this.rokTrajanja = rokTrajanja;
+	}
+	
+}
--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,5 +1,19 @@
 package magacin;
 
+import java.util.List;
+
+import artikal.Artikal;
+
 public class Magacin {
 
+	private List<Artikal> artikli;
+
+	public List<Artikal> getArtikli() {
+		return artikli;
+	}
+
+	public void setArtikli(List<Artikal> artikli) {
+		this.artikli = artikli;
+	}
+	
 }
